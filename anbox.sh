5514fffe2a48417739b1becfa427763918ca6548
Mattia990 <dyfg@hotmail.it>
2019-10-24 18:44:23 +0200

    Update anbox.sh and added possibility to run it

--- anbox.sh
+++ anbox.sh
@@ -1,9 +1,12 @@
-sudo apt install anbox-ubuntu-touch
+#!/bin/sh
+sudo mount -o remount,rw /
+sudo apt update
+sudo apt install -y anbox-ubuntu-touch
 mkdir anbox-data
 sudo wget -q --show-progress -O /home/phablet/anbox-data/android.img http://cdimage.ubports.com/anbox-images/android-armhf-64binder.img
 touch /home/phablet/anbox-data/.enable
-mkdir anbox-data/data
+mkdir /home/phablet/anbox-data/data
 sudo chmod -R o+wrx /home/phablet/anbox-data/data
 sudo start -q anbox-container
 start -q anbox-session
-echo please reboot device 2 times!
+echo please reboot device!
